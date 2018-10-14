6b7e3dc34ab837f456323c4a75da63300897be03
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-14 21:17:16 +0900

    Mockup components.

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Profile from '../Profile';
+import Links from '../Links';
+
+export default function App() {
+  return (
+    <div>
+      <Profile />
+      <Links />
+    </div>
+  );
+}
