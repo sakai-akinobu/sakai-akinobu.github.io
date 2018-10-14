ce1bcbd1bc8a269ad23305ae00f354ccaeebf5cc
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-14 21:40:44 +0900

    Fix profile component.

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import styles from './styles.scss';
+
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <section className={styles.container}>
+      <img className={styles.avatar} src="/images/avatar.jpg" />
+      <div>Akinobu Sakai</div>
+    </section>
+  );
 }
