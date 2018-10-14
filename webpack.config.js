325fcf0effa3de8fde6ec2a4dec6f9f34243bb72
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-14 20:57:25 +0900

    Bundle css.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -12,10 +13,53 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 2,
+                sourceMap: true,
+                localIdentName: '[local]___[hash:base64:5]',
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                outputStyle: 'expanded',
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: [
+                  require('autoprefixer')(),
+                ],
+              },
+            },
+          ],
+        }),
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.scss',
     ],
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'docs'),
   },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+  ],
 };
