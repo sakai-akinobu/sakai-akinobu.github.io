802e947333e7ed8426ca2dd272f8d88e34dacb24
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-14 22:05:04 +0900

    Fix links components.

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,5 +1,45 @@
 import React from 'react';
+import {
+  FaGithub,
+  FaNpm,
+  FaInstagram,
+  FaLastfm,
+} from 'react-icons/fa';
+
+import styles from './styles.scss';
+
+const links = [
+  {
+    component: FaGithub,
+    url: 'https://github.com/sakai-akinobu',
+  },
+  {
+    component: FaNpm,
+    url: 'https://www.npmjs.com/~sakai-akinobu',
+  },
+  {
+    component: FaInstagram,
+    url: 'https://www.instagram.com/sakai.akinobu/',
+  },
+  {
+    component: FaLastfm,
+    url: 'https://www.last.fm/ja/user/nobyu',
+  },
+];
 
 export default function Links() {
-  return <div>Links</div>;
+  return (
+    <section>
+      <ul className={styles.links}>
+        {links.map(({url, component}) => {
+          const Component = component;
+          return (
+            <li>
+              <a href={url}><Component /></a>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
